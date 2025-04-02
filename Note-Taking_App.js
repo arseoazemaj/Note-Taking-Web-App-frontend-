e7023d441912be8a0760262fc4dffb8eb896c79c
Dell <arseoazemaj@gmail.com>
2025-04-03 00:04:09 +0200

    more to work in the Log in and Sign up Pages

--- Note-Taking_App.js
+++ Note-Taking_App.js
@@ -18,3 +18,18 @@ function go_to_log_in(){
     document.getElementById("sign_up").style.display = "none";
     document.getElementById("log_ig").style.display = "block";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const password = document.getElementById("Password");
+    const confirmPassword = document.getElementById("confirmPassword");
+    const signUpBtn = document.getElementById("sign_up_btn");
+
+    function validatePasswords() {
+        const password = document.getElementById("Password").value;
+        const confirmPassword = document.getElementById("confirmPassword").value;
+        document.getElementById("sign_up_btn").disabled = password !== confirmPassword || password === "";
+    }
+
+    password.addEventListener("input", validatePasswords);
+    confirmPassword.addEventListener("input", validatePasswords);
+});
