d62542b8582de65ac872214065b607cb530ed900
Dell <arseoazemaj@gmail.com>
2025-04-02 14:56:53 +0200

    testing some things

---
+++ Note-Taking_App.ts
@@ -0,0 +1,5 @@
+console.log("Note added: This is your first note!");
+console.log("Note added: This is your second note!");
+console.log("Note added: This is your third note!");
+console.log("Note added: This is your fourth note!");
+console.log("Note added: This is your fifth note!");
