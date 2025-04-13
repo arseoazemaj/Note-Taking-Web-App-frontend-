73bfa367cc3f05d86ac5aa51520bc6de18b7e791
Dell <arseoazemaj@gmail.com>
2025-04-13 23:12:08 +0200

    push

---
+++ android/app/src/main/assets/public/Trash/Trash.js
@@ -0,0 +1,21 @@
+const menuOpener = document.getElementById("menuopener");
+const menuPage = document.getElementById("menu-page");
+
+let menuOpen = false;
+
+menuOpener.addEventListener("click", (e) => {
+    e.stopPropagation();
+    if (!menuOpen) {
+        menuPage.classList.remove("slide-out");
+        menuPage.classList.add("slide-in");
+        menuOpen = true;
+    }
+});
+
+document.addEventListener("click", (e) => {
+    if (menuOpen && !menuPage.contains(e.target)) {
+        menuPage.classList.remove("slide-in");
+        menuPage.classList.add("slide-out");
+        menuOpen = false;
+    }
+});
