f3f62ae4e1f234e849fe1d2c58d8a44f31e6771a
Dell <arseoazemaj@gmail.com>
2025-05-21 00:22:52 +0200

    better UX for authentication

---
+++ LoginSignup/script.js
@@ -0,0 +1,15 @@
+//TODO Delete in the end
+window.onload = function() {
+    window.location.href = "../Notes/Notes.html";
+}
+
+
+//TODO Uncomment in the end
+// const token = localStorage.getItem("token");
+
+// if (token) {
+//     window.location.href = "../Notes/Notes.html";
+// }
+// else {
+//     window.location.href = "../LoginSignuppages/Log_in-and-Sign_up.html";
+// }
