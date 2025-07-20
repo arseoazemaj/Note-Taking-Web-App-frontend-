348824c48a474ef544bd7db9940b18344c77858b
Dell <arseoazemaj@gmail.com>
2025-07-20 15:47:02 +0200

    impruving step by step

--- LoginSignup/Notes/Notes.js
+++ LoginSignup/Notes/Notes.js
@@ -207,6 +207,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 const move_menu = document.getElementById("move_menu");
 const more_options = document.getElementById("more_options");
 const create_folder_menu = document.getElementById("create_folder");
+const blur_background = document.getElementById("blur-background");
 
 function create_folder() {
     if (create_folder_menu.style.visibility !== 'visible') {
@@ -218,7 +219,7 @@ function create_folder() {
 function move() {
     if (move_menu) {
         move_menu.style.visibility = 'visible';
-        
+        blur_background.style.visibility = 'visible';
     }
 }
 
@@ -230,10 +231,20 @@ function more() {
     }
 }
 
+document.addEventListener("touchstart", function (event) {
+    if (!move_menu.contains(event.target)) {
+        move_menu.style.visibility = 'hidden';
+        more_options.style.visibility = 'hidden';
+        create_folder_menu.style.visibility = 'hidden';
+        blur_background.style.visibility = 'hidden';
+    }
+});
+
 function hideDecisions() {
         move_menu.style.visibility = 'hidden';
         more_options.style.visibility = 'hidden';
         create_folder_menu.style.visibility = 'hidden';
+        blur_background.style.visibility = 'hidden';
 
         SelectionMode = false;
 }
