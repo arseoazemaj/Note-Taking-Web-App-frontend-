cd99f626d0bf70c3d6ba065a74879491e2c70b52
arseo01 <arseoazemaj@gmail.com>
2026-01-24 00:56:24 +0100

    different thingfs (changes for the UI of course)

--- Seluna_frontend/Profile/Profile.js
+++ Seluna_frontend/Profile/Profile.js
@@ -1,10 +1,21 @@
 const back_2 = document.getElementById("back_2");
 const setting_page = document.getElementById("setting_page");
+const username_edit = document.getElementById("username_edit");
+const blur_background = document.getElementById("blur_background");
 
 function open_settings() {
     setting_page.style.visibility = "visible";
 }
 
 function change_name() {
+    blur_background.style.visibility = "visible";
+    username_edit.classList.remove("hide");
+    username_edit.classList.add("show");
     console.log("changing name...");
+}
+
+function blur_backgroundHandler() {
+    blur_background.style.visibility = "hidden";
+    username_edit.classList.add("hide");
+    username_edit.classList.remove("show");
 }
