6c4bb48224136f76eca5a2ac0e928f0ae68e0e2d
arseo01 <arseoazemaj@gmail.com>
2026-01-24 01:50:10 +0100

    working for the Username (being able to change it)

--- Seluna_frontend/Profile/Profile.js
+++ Seluna_frontend/Profile/Profile.js
@@ -2,6 +2,7 @@ const back_2 = document.getElementById("back_2");
 const setting_page = document.getElementById("setting_page");
 const username_edit = document.getElementById("username_edit");
 const blur_background = document.getElementById("blur_background");
+const continue_edit = document.getElementById("continue_edit");
 
 function open_settings() {
     setting_page.style.visibility = "visible";
@@ -11,10 +12,17 @@ function change_name() {
     blur_background.style.visibility = "visible";
     username_edit.classList.remove("hide");
     username_edit.classList.add("show");
+    continue_edit.disabled = true;
     console.log("changing name...");
 }
 
 function blur_backgroundHandler() {
+    blur_background.style.visibility = "hidden";
+    username_edit.classList.add("hide");
+    username_edit.classList.remove("show");
+}
+
+function cancel_change() {
     blur_background.style.visibility = "hidden";
     username_edit.classList.add("hide");
     username_edit.classList.remove("show");
