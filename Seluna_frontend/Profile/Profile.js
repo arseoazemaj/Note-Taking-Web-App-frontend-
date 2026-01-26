31480f2e119b4480337f146f9bab7619077f7f08
arseo01 <arseoazemaj@gmail.com>
2026-01-26 14:31:20 +0100

    made the request to get the username from the db and will make the other features (working on the one to change username)

--- Seluna_frontend/Profile/Profile.js
+++ Seluna_frontend/Profile/Profile.js
@@ -1,21 +1,45 @@
+const username = document.getElementById("username");
 const setting_page = document.getElementById("setting_page");
-const username_text = document.getElementById("username");
-const username_edit = document.getElementById("username_edit");
-const blur_background = document.getElementById("blur_background");
-const continue_edit = document.getElementById("continue_edit");
 
 document.addEventListener("DOMContentLoaded", loadUsername );
 
-function loadUsername() { //*Will get the username from the DB
-    const username_data = "user_333333";
+async function loadUsername() {
+    try {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            console.error("No JWT token found");
+            return;
+        }
 
+        const response = await fetch("http://localhost:5216/api/account/username", {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                }
+            }
+        );
 
-    username_text.textContent = username_data;
-    console.log("Username is:", username_data);
+        if (!response.ok) {
+            throw new Error(`HTTP ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        username.textContent = data.username;
+        console.log("Username is:", data.username);
+
+    } catch (error) {
+        console.error("Failed to load username:", error);
+    }
 }
 
 function account() {
     console.log("Account settings opened");
     const account = document.getElementById("account");
     account.style.backgroundColor = "#251e45";
+}
+
+function change_name() {
+    console.log("Change name clicked");
 }
