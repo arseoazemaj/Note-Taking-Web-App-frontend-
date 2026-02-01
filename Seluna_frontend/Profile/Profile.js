b3d3d3addd58b093ca6c3143331a9e5bfac481f5
arseo01 <arseoazemaj@gmail.com>
2026-02-01 15:20:29 +0100

    made the menus to change password (a little messy)

--- Seluna_frontend/Profile/Profile.js
+++ Seluna_frontend/Profile/Profile.js
@@ -67,34 +67,6 @@ async function loadEmail() {
     }
 }
 
-const account_settings = document.getElementById("account_settings");
-const account_btn = document.getElementById("account");
-const note_settings = document.getElementById("note_settings");
-const note_btn = document.getElementById("note");
-
-const backup_settings = document.getElementById("backup_settings");
-const backup_btn = document.getElementById("backup");
-
-const security_settings = document.getElementById("security_settings");
-const security_btn = document.getElementById("security");
-
-const help_settings = document.getElementById("help_settings");
-const help_btn = document.getElementById("help");
-
-function account() {
-    account_settings.style.visibility = "visible";
-    note_settings.style.visibility = "hidden";
-    backup_settings.style.visibility = "hidden";
-    security_settings.style.visibility = "hidden";
-    help_settings.style.visibility = "hidden";
-
-    account_btn.style.backgroundColor = "#251e45";
-    note_btn.style.backgroundColor = "transparent";
-    backup_btn.style.backgroundColor = "transparent";
-    security_btn.style.backgroundColor = "transparent";
-    help_btn.style.backgroundColor = "transparent";
-}
-
 async function change_name() {
     try {
         const token = localStorage.getItem("token");
@@ -151,13 +123,160 @@ async function change_email() {
     }
 }
 
+const blur_background = document.getElementById("blur_background");
+const password_change_menu = document.getElementById("password_change_menu");
+
+
+function blur_backgroundHandler() {
+    setTimeout(() => {
+        if (password_change_menu.classList.contains("show")) {
+            password_change_menu.classList.add("hide");
+            password_change_menu.classList.remove("show");
+            username_input.style.visibility = "visible";
+            email_input.style.visibility = "visible";
+            const oldPassword_input = document.getElementById("old_password");
+            oldPassword_input.value = "";
+            oldPassword_input.type = "password";
+            const eyeIcon = document.getElementById("eye_2");
+            const hiddenEyeIcon = document.getElementById("hidden_eye_2");
+            eyeIcon.style.display = "inline";
+            hiddenEyeIcon.style.display = "none";
+        }
+
+        blur_background.style.visibility = "hidden";
+    }, 100);
+}
+
+const account_settings = document.getElementById("account_settings");
+const account_btn = document.getElementById("account");
+const note_settings = document.getElementById("note_settings");
+const note_btn = document.getElementById("note");
+
+const backup_settings = document.getElementById("backup_settings");
+const backup_btn = document.getElementById("backup");
+
+const security_settings = document.getElementById("security_settings");
+const security_btn = document.getElementById("security");
+
+const help_settings = document.getElementById("help_settings");
+const help_btn = document.getElementById("help");
+
+function account() {
+    account_settings.style.visibility = "visible";
+    note_settings.style.visibility = "hidden";
+    backup_settings.style.visibility = "hidden";
+    security_settings.style.visibility = "hidden";
+    help_settings.style.visibility = "hidden";
+
+    account_btn.style.backgroundColor = "#251e45";
+    note_btn.style.backgroundColor = "transparent";
+    backup_btn.style.backgroundColor = "transparent";
+    security_btn.style.backgroundColor = "transparent";
+    help_btn.style.backgroundColor = "transparent";
+}
+
+const username_input = document.getElementById("username_input");
+const email_input = document.getElementById("email_input");
+
+function change_password() {
+    username_input.style.visibility = "hidden";
+    email_input.style.visibility = "hidden";
+
+    password_change_menu.classList.add("show");
+    password_change_menu.classList.remove("hide");
+    blur_background.style.visibility = "visible";
+}
+
+function cancel_password() {
+    password_change_menu.classList.add("hide");
+    password_change_menu.classList.remove("show");
+    username_input.style.visibility = "visible";
+    email_input.style.visibility = "visible";
+    const oldPassword_input = document.getElementById("old_password");
+    oldPassword_input.value = "";
+    oldPassword_input.type = "password";
+    const eyeIcon = document.getElementById("eye_2");
+    const hiddenEyeIcon = document.getElementById("hidden_eye_2");
+    eyeIcon.style.display = "inline";
+    hiddenEyeIcon.style.display = "none";
+    blur_backgroundHandler();
+}
+
+const oldPassword = document.getElementById("old_password");
+const continuePasswordBtn = document.getElementById("continue_password");
+
+continuePasswordBtn.disabled = true;
+
+oldPassword.addEventListener("input", oldPasswordValidation);
+
+function oldPasswordValidation() {
+    const password = oldPassword.value;
+
+    const longEnough = password.length >= 8;
+    const noSpaces = !password.includes(" ");
+
+    if (longEnough && noSpaces) {
+        continuePasswordBtn.disabled = false;
+    } else {
+        continuePasswordBtn.disabled = true;
+    }
+}
+
+const change_password_menu = document.getElementById("change_password");
+
+function continue_password() {
+    console.log("Password chencked");
+
+    const oldPassword_input = document.getElementById("old_password").value.trim();
+    const old_password = sanitize(oldPassword_input);
+    console.log(old_password);
+
+    change_password_menu.classList.add("show");
+    change_password_menu.classList.remove("hide");
+    password_change_menu.classList.add("hide");
+    password_change_menu.classList.remove("show");
+}
+
+function cancel_password_change() {
+    change_password_menu.classList.add("hide");
+    change_password_menu.classList.remove("show");
+    password_change_menu.classList.add("show");
+    password_change_menu.classList.remove("hide");
+    oldPassword.value = "";
+    continuePasswordBtn.disabled = true;
+}
 
 
 
 
+function togglePassword(event) {
+    const toggle = event.target.closest("span");
+    if (!toggle) return;
+
+    const wrapper = toggle.closest(".password-wrapper");
+    if (!wrapper) return;
+
+    const passwordInput = wrapper.querySelector("input");
+
+    const eyeIcon = toggle.querySelector(".eye_icon");
+    const hiddenEyeIcon = toggle.querySelector(".hidden_eye_icon");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.style.display = "none";
+        hiddenEyeIcon.style.display = "inline";
+    } else {
+        passwordInput.type = "password";
+        eyeIcon.style.display = "inline";
+        hiddenEyeIcon.style.display = "none";
+    }
+}
 
 
-//* Here will me all functions for the account settings menu
+
+//* Here will be all functions for the account settings menu
+
+
 
 function note() {
     account_settings.style.visibility = "hidden";
